094db8096f79e5f0b29728cd25d7ea4a7fef78ac
claudioed <claudioed.oliveira@gmail.com>
2024-08-20 07:29:09 -0300

    remove kafka-streams implementation

--- src/main/java/com/paymentic/domain/LastUserTransaction.java
+++ src/main/java/com/paymentic/domain/LastUserTransaction.java
@@ -6,27 +6,37 @@ import java.math.BigDecimal;
 import org.apache.kafka.common.protocol.types.Field.Str;
 
 public class LastUserTransaction {
+
   private String document;
+
   private BigDecimal value;
+
   private String currency;
+
   private String sellerId;
+
   public LastUserTransaction() {
   }
+
   public LastUserTransaction(String document, BigDecimal value, String currency, String sellerId) {
     this.document = document;
     this.value = value;
     this.currency = currency;
     this.sellerId = sellerId;
   }
+
   public String getDocument() {
     return this.document;
   }
+
   public BigDecimal getValue() {
     return this.value;
   }
+
   public String getCurrency() {
     return this.currency;
   }
+
   public String getSellerId() {
     return sellerId;
   }
--- src/main/java/com/paymentic/domain/application/UserTransactionService.java
+++ src/main/java/com/paymentic/domain/application/UserTransactionService.java
@@ -12,13 +12,17 @@ import java.time.LocalDateTime;
 
 @ApplicationScoped
 public class UserTransactionService {
+
   private final UserTransactionRepository userTransactionRepository;
+
   public UserTransactionService(UserTransactionRepository userTransactionRepository) {
     this.userTransactionRepository = userTransactionRepository;
   }
+
   public void storeTransaction(@Observes UserTransaction transaction) {
     this.userTransactionRepository.add(transaction);
   }
+
   public AverageTransactionsValue totalPerMonth(String document, LocalDate at) {
     return this.userTransactionRepository.totalPerMonth(document, at);
   }
